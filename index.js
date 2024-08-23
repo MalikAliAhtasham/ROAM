d94cad60fe3bcdb8623bf5e2fe2721861c4a8355
MalikAliAhtasham <malikali13691369@gmail.com>
2024-08-23 19:16:16 +0500

    All html files uploaded

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var spinner = document.getElementById('spinner');
+    var content = document.querySelector('.content');
+
+    console.log("Page loaded, showing spinner.");
+
+    content.classList.add('hide-content');
+    spinner.classList.add('show');
+    
+    setTimeout(function() {
+        console.log("Timeout reached, hiding spinner.");
+        spinner.classList.remove('show');
+        content.classList.remove('hide-content');
+    }, 1000);
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-fixed');
+    } else {
+        navbar.classList.remove('navbar-fixed');
+    }
+});
+
+
+
